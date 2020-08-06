95a433a2b98a7208c42eb0c7619e90716b9f7f3f
Kiarash Kiani <hi@kiarash.info>
2020-08-06 17:37:13 +0430

    change hws service to use rabitmq

---
+++ packages/services/hws/hardware_service.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import pika
+import serial
+
+
+arduino_address = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_854333332313515052D0-if00"
+arduino_serial_connection = serial.Serial(arduino_address, 9600, timeout=0.5)
+
+rabbitmq_connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+rabbitmq_channel = rabbitmq_connection.channel()
+
+rabbitmq_channel.queue_declare(queue='com.shannon.hws')
+
+
+def callback(ch, method, properties, body):
+    if body == b'lamp off':
+        arduino_serial_connection.write("2")
+    elif body == b'lamp on':
+        arduino_serial_connection.write("3")
+    elif body == b'door lock':
+        arduino_serial_connection.write("5")
+    elif body == b'door unlock':
+        arduino_serial_connection.write("4")
+    else:
+        print(f' [!] unknown command: {body}')
+
+
+rabbitmq_channel.basic_consume(queue='com.shannon.hws', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+rabbitmq_channel.start_consuming()
