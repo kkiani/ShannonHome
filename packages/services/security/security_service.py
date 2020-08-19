cc0d63dacdc134687014d0d61f1166ea8f1d51ed
Kiarash Kiani <hi@kiarash.info>
2020-08-19 16:56:22 +0430

    fix security isssue

--- packages/services/security/security_service.py
+++ packages/services/security/security_service.py
@@ -25,25 +25,25 @@ class SecurityService(SHServiceConsumer):
             self.handle_failed_token()
         elif message == SecurityEvent.SECURITY_OFF.value:
             self.is_enable = False
-            self.__push.send_message('Security alert', 'System security service is turned off')
+            self._push.send_message('Security alert', 'System security service is turned off')
         elif message == SecurityEvent.SECURITY_ON.value:
             self.is_enable = True
-            self.__push.send_message('Security alert', 'System security service is turned on')
+            self._push.send_message('Security alert', 'System security service is turned on')
         else:
             self.handle_unkown_event(message)
     
     # event handlers
     def handle_break_in(self):
-        self.__push.send_message('Security alert', 'someone is in your room.')
+        self._push.send_message('Security alert', 'someone is in your room.')
 
     def handle_failed_token(self):
-        self.__push.send_message('Security alert', 'a failed attempt with broken token to chenge system controls happend.')
+        self._push.send_message('Security alert', 'a failed attempt with broken token to chenge system controls happend.')
 
     def handle_failed_login(self):
-        self.__push.send_message('Security alert', 'a failed loggin attempt has been occurred.')
+        self._push.send_message('Security alert', 'a failed loggin attempt has been occurred.')
 
     def handle_unkown_event(self, event_message):
-        self.__push.send_message('Security alert', 'System recived an unkown security event.')
+        self._push.send_message('Security alert', 'System recived an unkown security event.')
         logging.warning('SecurityService: Unkown Event: {}'.format(event_message))
 
 security = SecurityService()
